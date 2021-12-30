8deba361adcdf13166d1941f8bbb8e4b361688c6
Brian Turner <bturner@texascdlprep.com>
2021-12-29 20:14:18 -0600

    Done with major part of coding just some minimal style needs to be done

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -16,6 +16,44 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.post('/signup', async (req, res) => {
+  try {
+    const userName = await User.findOne({ where: { username: req.body.username } });
+    if (!userName) {
+      res
+        .status(400)
+        .json({ message: 'Name required, please enter your name!' });
+      return;
+    }
+
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Enter a correct email address, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
